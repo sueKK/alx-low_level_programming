22d035e7bbc5d36d34d612e78bfddf2b81db86eb
sueKK <suzzykawira6@gmail.com>
2023-09-04 13:53:03 -0700

    Neither irony nor....

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two numbers
+ * @argv: argument vector
+ * @argc: argument count
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
