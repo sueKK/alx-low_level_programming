5415e81c8fdb3c4b0e3cb21ff4ab44b5a5e07e09
sueKK <suzzykawira6@gmail.com>
2023-09-06 12:17:25 -0700

    Float like a butterfly

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,27 +1,24 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * create_array - creates array
+ * create_array - create array of size size and assign char c
  * @size: size of array
- * @c: character to store in array
- * Return: pointer to the array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * Return: pointer to array, NULL if fail
+ *
  */
-
 char *create_array(unsigned int size, char c)
 {
+	char *str;
 	unsigned int i;
-	char *arr;
 
-	if (size == 0)
-		return (NULL);
-
-	arr = malloc(sizeof(char) * size);
-
-	if (arr == NULL)
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		arr[i] = c;
-	return (arr);
+		str[i] = c;
+	return (str);
 }
 
